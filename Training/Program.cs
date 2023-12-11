d3bde95f895b13c60635011c1496210be1cf49d8
Artur-Leer <153430292+Artur-Leer@users.noreply.github.com>
2023-12-11 16:01:02 +0100

    - Improve Console Output - Add Output when playerChoice-input not matching any index of choices

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,55 +1,54 @@
 ﻿// C# BASIC STRUCTURE
 using System;
+// TYPZUWEISUNG + VARIABLEN-DEFINIERUNG
+string playerName;
+string[] choices = { "Schere", "Stein", "Papier", "Echse", "Spock" };
+Random random = new Random();
 
-namespace SchereSteinPapier
+// ERMÖGLICHEN SIE DIE EINGABE DES BENUTZERNAMENS
+Console.WriteLine("Schön dass du hier bist! Wie lautet dein Name? :");
+playerName = Console.ReadLine();
+
+while (true)
 {
-    class Spiel
+    // ERMÖGLICHEN SIE DIE AUSWAHL DES SPIELZUGES
+    Console.WriteLine(playerName + ", bitte Wähle: Schere, Stein, Papier, Echse oder Spock");
+    string playerChoice = Console.ReadLine();
+
+    if (Array.IndexOf(choices, playerChoice) == -1)
     {
-        static void Main(string[] args)
-        {
-            // TYPZUWEISUNG + VARIABLEN-DEFINIERUNG
-            string playerName;
-            string[] choices = { "Schere", "Stein", "Papier", "Echse", "Spock" };
-            Random random = new Random();
+        Console.WriteLine("Ungültige Eingabe, versuche es noch einmal!");
+        continue;
+    }
 
-            // ERMÖGLICHEN SIE DIE EINGABE DES BENUTZERNAMENS
-            Console.WriteLine("Wie heißt du?:");
-            playerName = Console.ReadLine();
+    string computerChoice = choices[random.Next(choices.Length)];
+    Console.WriteLine("Der Computer wählt: " + computerChoice);
 
-            while (true)
-            {
-                // ERMÖGLICHEN SIE DIE AUSWAHL DES SPIELZUGES
-                Console.WriteLine("Wähle: Schere, Stein, Papier, Echse oder Spock");
-                string playerChoice = Console.ReadLine();
+    // UNENTSCHIEDEN PRÜFUNG
+    if (playerChoice == computerChoice)
+    {
+        Console.WriteLine("Unentschieden!");
+    }
 
-                string computerChoice = choices[random.Next(choices.Length)];
-                Console.WriteLine("Der Computer wählt: " + computerChoice);
-
-                // UNENTSCHIEDEN PRÜFUNG
-                if (playerChoice == computerChoice)
-                {
-                Console.WriteLine("Unentschieden!");
-                }
-            
-                // DEFINIERE FÄLLE IN DEM DER SPIELER GEWINNT // ÜBERSETZT: SONST WENN EINER DER FOLGENDEN FÄLLE TRUE IST, AUSGABE DASS SPIELER GEWONNEN HAT//
-                // ES WERDEN NUR FÄLLE BETRACHTET IN DENEN DER SPIELER GEWINNT; ALLE ANDEREN FÄLLE BEDEUTEN DASS COMPUTER GEWINNT
-                else if ((playerChoice == "Schere" && (computerChoice == "Papier" || computerChoice == "Echse")) ||
-                        (playerChoice == "Papier" && (computerChoice == "Stein" || computerChoice == "Spock")) ||
-                        (playerChoice == "Stein" && (computerChoice == "Echse" || computerChoice == "Schere")) ||
-                        (playerChoice == "Echse" && (computerChoice == "Spock" || computerChoice == "Papier")) ||
-                        (playerChoice == "Spock" && (computerChoice == "Schere" || computerChoice == "Stein")))
-                {
-                Console.WriteLine(playerName + " gewinnt!");
-                }
-                // FÜR ALLE ANDEREN FÄLLE -> COMPUTER GEWINNT
-                else
-                {
-                Console.WriteLine("Computer gewinnt!");
-                }
-                Console.WriteLine("Möchten Sie noch einmal spielen? (Ja/Nein)");
-                if (Console.ReadLine() != "Ja") // FALLS EINGABE ANDERS ALS "Ja" -> Brechen der while(true) Loop und Ende des Programms
-                break;
-            }
-        }
+    // DEFINIERE FÄLLE IN DEM DER SPIELER GEWINNT // ÜBERSETZT: SONST WENN EINER DER FOLGENDEN FÄLLE TRUE IST, AUSGABE DASS SPIELER GEWONNEN HAT//
+    // ES WERDEN NUR FÄLLE BETRACHTET IN DENEN DER SPIELER GEWINNT; ALLE ANDEREN FÄLLE BEDEUTEN DASS COMPUTER GEWINNT
+    else if ((playerChoice == "Schere" && (computerChoice == "Papier" || computerChoice == "Echse")) ||
+            (playerChoice == "Papier" && (computerChoice == "Stein" || computerChoice == "Spock")) ||
+            (playerChoice == "Stein" && (computerChoice == "Echse" || computerChoice == "Schere")) ||
+            (playerChoice == "Echse" && (computerChoice == "Spock" || computerChoice == "Papier")) ||
+            (playerChoice == "Spock" && (computerChoice == "Schere" || computerChoice == "Stein")))
+    {
+        Console.WriteLine(playerName + " gewinnt!");
+    }
+    // FÜR ALLE ANDEREN FÄLLE -> COMPUTER GEWINNT
+    else
+    {
+        Console.WriteLine("Leider hat der Computer dieses mal gewonnen!");
+    }
+    Console.WriteLine("Möchtest noch einmal spielen? (Ja/Nein)");
+    if (Console.ReadLine() != "Ja") // FALLS EINGABE ANDERS ALS "Ja" -> Brechen der while(true) Loop und Ende des Programms
+    {
+        Console.WriteLine("Vielen Dank für's Spielen " + playerName + ". Bis zum nächsten mal!");
+        break;
     }
 }
